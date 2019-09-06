a9ee130e7d687a27c3623dd04ddb78757c1fe439
Sam Gold <SamGold8556@gmail.com>
2019-09-06 11:11:42 -0600

    Finished Player and Vehicle Movement Mechanics Now beginning Car Entering Mechanics

---
+++ Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour
+{  
+
+public List<AxleInfo> axleInfos; // the information about each individual axle
+public float maxMotorTorque; // maximum torque the motor can apply to wheel
+public float maxSteeringAngle; // maximum steer angle the wheel can have
+public GameObject carCamera; // controls the camera attached to the car
+public bool isCarActive; // determines whether or not player is inside car
+
+
+
+
+    private void Start()
+    {
+        isCarActive = false;    
+    }
+
+    public void FixedUpdate()
+{
+        if (isCarActive)
+        {
+            float motor = maxMotorTorque * Input.GetAxis("Vertical");
+            float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                if (axleInfo.steering)
+                {
+                    axleInfo.leftWheel.steerAngle = steering;
+                    axleInfo.rightWheel.steerAngle = steering;
+                }
+                if (axleInfo.motor)
+                {
+                    axleInfo.leftWheel.motorTorque = motor;
+                    axleInfo.rightWheel.motorTorque = motor;
+                }
+            }
+        }
+}
+
+    public void Update()
+    {
+        CarActivityCheck();
+    }
+
+    public void CarActivityCheck()
+    {
+        if (!isCarActive)
+        {
+            carCamera.SetActive(false);
+        } else
+        {
+            carCamera.SetActive(true);
+        }
+    }
+
+}
+    
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
--- Assets/Scripts/ThirdPersonCharacterControl.cs
+++ Assets/Scripts/ThirdPersonCharacterControl.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class ThirdPersonCharacterControl : MonoBehaviour
 {
+    public GameObject playerCamera;
     public float Speed;
+    public bool inCar;
+    public bool nearCar;
+    public GameObject carToEnter;
 
-	void Update ()
+    private void Start()
+    {
+    }
+
+    void Update ()
     {
         PlayerMovement();
     }
 
     void PlayerMovement()
     {
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
-        transform.Translate(playerMovement, Space.Self);
+        if (!inCar)
+        {
+            float hor = Input.GetAxis("Horizontal");
+            float ver = Input.GetAxis("Vertical");
+            Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+        }      
+    }
+
+    public void EnterCar(SimpleCarController activeCar)
+    {
+        activeCar.isCarActive = true;
+        playerCamera.SetActive(false);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Vehicle")
+        {
+            nearCar = true;
+            Debug.Log("Near Car");
+            //EnterCar(carToEnter);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Enter Car");
+                gameObject.SetActive(false);
+                //EnterCar(other.gameObject.GetComponent<SimpleCarController>());
+            }
+        }
     }
 }
