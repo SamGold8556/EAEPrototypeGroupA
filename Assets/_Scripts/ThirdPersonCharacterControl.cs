942eee7e9c396b5c85edd5ff24cd4f5372d36513
Ben Rose <u0916753@ad.utah.edu>
2019-09-06 16:15:18 -0600

    Hopefully fixed some loading issues.

---
+++ Assets/_Scripts/ThirdPersonCharacterControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterControl : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public float Speed;
+    public bool inCar;
+    public bool nearCar;
+    public GameObject carToEnter;
+
+    private void Start()
+    {
+    }
+
+    void Update ()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if (!inCar)
+        {
+            float hor = Input.GetAxis("Horizontal");
+            float ver = Input.GetAxis("Vertical");
+            Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+            if (nearCar && Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Enter Car");
+                GameManager.instance.EnterCar();
+                //gameObject.SetActive(false);
+                //EnterCar(other.gameObject.GetComponent<SimpleCarController>());
+            }
+            nearCar = false;
+        }      
+    }
+
+    public void EnterCar(SimpleCarController activeCar)
+    {
+        activeCar.isCarActive = true;
+        playerCamera.SetActive(false);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Vehicle")
+        {
+            nearCar = true;
+            Debug.Log("Near Car");
+            //EnterCar(carToEnter);
+        }
+    }
+}
